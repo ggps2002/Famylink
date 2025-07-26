c8420bad421da44bcd3fdb200ec9917428452c85
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2025-07-26 05:32:28 +0530

    change env

--- frontend/src/Config/url.jsx
+++ frontend/src/Config/url.jsx
@@ -1,1 +1,1 @@
-export const BACKEND_API_URL = import.meta.env.MODE === 'production' ? "https://famylink.onrender.com" : "http://localhost:3000"
+export const BACKEND_API_URL = import.meta.env.VITE_API_BASE_URL;
